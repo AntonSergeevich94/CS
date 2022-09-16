a4a103bc23952f6da4793a91dc13a788798f4369
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-16 23:10:11 +0300

    Done 1 task

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое число 1  ");
+
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите целое число 2  ");
+
+int number2 = int.Parse(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}

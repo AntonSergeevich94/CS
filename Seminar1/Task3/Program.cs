273c72acf02371c0a7d15dd936e35a9e058e2194
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-16 23:14:13 +0300

    Done 3 task

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое число  ");
+
+double number1 = double.Parse(Console.ReadLine());
+
+double number2 = -(number1);
+
+while (number2 <= number1)
+{
+    Console.Write(number2+" ");
+    number2 = number2+1;
+} 

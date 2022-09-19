96e61d676d3af711a957da629b704e616cb60946
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-19 19:47:17 +0300

    create example 010

---
+++ Lection2/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1,12,118,4,15,16,17,118};
+
+int n = array.Length;
+int find = 118;
+
+int index = 0;
+
+while (index<n)
+{
+if(array[index] == find)
+{
+    Console.WriteLine(index);
+    break;
+}
+    index++;
+}

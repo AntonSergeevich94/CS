3347747545e6f06a1d86219e8088c7e2681db9f6
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-19 21:01:28 +0300

    create task2_3

---
+++ Seminar2/Task2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа");
+Console.Write("Введите число a ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число b ");
+int num2 = int.Parse(Console.ReadLine());
+int a = num1;
+int b = num2;
+if (b>a)
+{
+    a = num2;
+    b = num1;
+}
+if (a % b == 0 ) 
+{
+    Console.WriteLine($"Число {a} кратно {b}");
+}
+else
+{
+Console.WriteLine($"Число {a} не кратно {b} остаток от деления {a % b}");
+}

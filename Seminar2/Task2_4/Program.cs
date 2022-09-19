3cad75bc684b913ebfccc7ffdde0d4cc0ec634bb
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-19 21:20:27 +0300

    create task2_4

---
+++ Seminar2/Task2_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+if (a % (7*23) == 0 ) 
+{
+    Console.WriteLine($"Число {a} кратно 7 и 23");
+}
+else
+{
+Console.WriteLine($"Число {a} не кратно 7 и 23 остаток от деления {a % (7*2)}");
+}

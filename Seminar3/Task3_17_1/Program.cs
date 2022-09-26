4775abb0c1f7b28821b670b1f2325249db57069c
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-26 21:24:29 +0300

    done task3_17_1

---
+++ Seminar3/Task3_17_1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y: ");
+int y = int.Parse(Console.ReadLine());
+
+FindChetvert(x,y);
+FindChetvert(x+10,y+10);
+
+
+
+
+void FindChetvert(int x, int y){
+if (x>0 && y>0){
+    Console.WriteLine("1");
+}
+else if (x<0 && y>0){
+    Console.WriteLine("2");
+}
+else if (x<0 && y<0){
+    Console.WriteLine("3");
+}
+else {
+    Console.WriteLine("4");
+}
+}

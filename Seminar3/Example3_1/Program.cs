6e85912c660c6357d3606fc4e5baf480a2624c6c
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-26 21:04:45 +0300

    done example3_1

---
+++ Seminar3/Example3_1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вариант 2 решения для задачи с числами
+Console.Clear();
+Console.WriteLine("Введите число ");
+int numb = int.Parse(Console.ReadLine());
+string numbr = numb.ToString();
+int a = numbr.Length;
+Console.WriteLine($"Число {numb} длинна {a} третий символ {Math.Abs(int.Parse(numbr)).ToString()[2]}");
+
+//

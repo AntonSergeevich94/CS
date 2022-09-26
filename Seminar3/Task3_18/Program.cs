106deb6a8b4ea4c1068bd3f1d703f1eea3e998d3
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-26 21:38:01 +0300

    done task3_18

---
+++ Seminar3/Task3_18/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите четверть ");
+int a = int.Parse(Console.ReadLine());
+
+Chetvert(a);
+
+void Chetvert(int a){
+if (a == 1){
+    Console.WriteLine("Диапазон первой четверти X>0, Y>0");
+}
+else if (a == 2){
+    Console.WriteLine("Диапазон второй четверти X<0, Y>0");
+}
+else if (a ==3){
+    Console.WriteLine("Диапазон третей четверти X<0, Y<0");
+}
+else {
+    Console.WriteLine("Диапазон четвертой четверти X>0, Y<0");
+}
+}

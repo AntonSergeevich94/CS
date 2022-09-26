9917ac98f7627e887cdbda7fe4d6df77c452706d
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-09-26 21:50:05 +0300

    done task3_18_1

---
+++ Seminar3/Task3_18_1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите четверть ");
+
+int num = Convert.ToInt16(Console.ReadLine());
+    switch (num)
+    {
+        case 1:
+        Console.WriteLine("Диапазон первой четверти X>0, Y>0");
+        break;
+        case 2:
+        Console.WriteLine("Диапазон второй четверти X<0, Y>0");
+        break;
+        case 3:
+        Console.WriteLine("Диапазон третей четверти X<0, Y<0");
+        break;
+        case 4:
+        Console.WriteLine("Диапазон четвертой четверти X>0, Y<0");
+        break;
+        default:
+        Console.WriteLine("Введите заново");
+        break;
+    }
+

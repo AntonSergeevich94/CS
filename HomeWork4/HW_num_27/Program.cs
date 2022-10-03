6b1a1d873465fa0ba269a5c17fe50079a3832211
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-10-03 19:14:41 +0300

    done task 27

---
+++ HomeWork4/HW_num_27/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12 */
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма цифр в числе {a} = {SumC(a)}");
+
+int SumC(int a)
+{
+    int sum =0;
+    while (a>0)
+    {
+        sum=sum+a%10;
+        a=a/10;
+    }
+    return sum;
+}

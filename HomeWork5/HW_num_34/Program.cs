30f669a60a78a3e0d10d5374ac4e5825250f5bc1
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-10-17 16:35:27 +0300

    done 34

---
+++ HomeWork5/HW_num_34/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] NewArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int SkolkoChetNumbers(int[] array)
+{
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            c++;
+        }
+
+    }
+    return c;
+}
+
+int[] massive = NewArray(20, 100, 999);
+int Chet = SkolkoChetNumbers(massive);
+Console.WriteLine($"[{String.Join(", ", massive)}]");
+Console.Write($"Четных чисел в массиве: {Chet}");

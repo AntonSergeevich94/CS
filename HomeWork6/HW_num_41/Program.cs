ff7ce179116f63586ba91a8636b24846a65413cb
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-10-17 18:23:51 +0300

    done 41

---
+++ HomeWork6/HW_num_41/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Сколько чисел будем вводить? (Введите число М)");
+int size = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Введите {size} чисел");
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) k = k + 1;
+
+    }
+Console.WriteLine($"Среди чисел [{String.Join(", ", array)}] {k} больше 0");

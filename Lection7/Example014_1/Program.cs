8d97385eb0fd54b898d05298bc2108f6f39c490d
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-10-17 13:11:12 +0300

    7_1

---
+++ Lection7/Example014_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Собрать строку с числами от a до b, a<=b
+
+Console.Clear();
+// Через цикл
+/*string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <=b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string chisla = NumbersFor(3,10);
+Console.WriteLine(chisla);*/
+
+// Через Рекурсию
+string NumbersRec(int a, int b)
+{
+    if (a<=b) return $"{a} "+NumbersRec(a+1,b);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(1,10));

f927f7e69a214b7ecd282342c75c34f32c4cb9a1
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-10-17 18:57:22 +0300

    done 52

---
+++ HomeWork7/HW_num_52/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int rows = new Random().Next(1, 10);
+int columns = new Random().Next(1, 10);
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+double[] srar = SrArifm(array);
+Console.WriteLine($"[{String.Join(", ", srar)}]");
+
+
+
+int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+    int[,] result = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] SrArifm(int[,] array)
+{
+    double[] srAr = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        srAr[j] = (double) sum / array.GetLength(0);
+    }
+    return srAr;
+}

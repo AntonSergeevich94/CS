407d188f07b439431accc554de36805c2cf54627
Anton Sergeevich <lebedev.anton94@gmail.com>
2022-10-20 10:56:39 +0300

    done 66

---
+++ HomeWork9/HW_num_66/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+
+Console.Write("Введите минимальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите максимальное число:  ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($" Сумма чисел от {m} до {n} равно {SumFromMtoN(m,n)}");
+
+int SumFromMtoN(int numberMIN, int numberMAX)
+{
+    if (numberMAX == numberMIN) return numberMIN;
+    return (numberMAX + SumFromMtoN(numberMIN, numberMAX - 1));
+}
